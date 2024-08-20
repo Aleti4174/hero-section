7cf883ad79db419e692e39a01e7e4cf67a971114
Aleti4174 <pannalapreethi8@gmail.com>
2024-08-20 14:04:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('hamburger-menu').addEventListener('click', function ()
+{
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+  });
